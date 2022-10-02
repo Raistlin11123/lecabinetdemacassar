2cb74b2b1122d25be7d8397cbc5a639f2a88470c
Philippe Pavec <philippepavec@gmail.com>
2022-10-02 15:30:53 +0200

    e

--- lecabinetdemacassar/settings.py
+++ lecabinetdemacassar/settings.py
@@ -155,3 +155,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Simplified static file serving.
 # https://pypi.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings.dev')
