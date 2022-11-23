ee6a3c0fb82e0669ebe463868009690103c1b8f7
Pavec Philippe <philippe.pavec@student-cs.fr>
2022-11-23 09:45:22 +0100

    whitenoiseé

--- lecabinetdemacassar/settings.py
+++ lecabinetdemacassar/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '3d7bc24cbcba5262a9f755215cb1778ae9a6d89c85924af6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','lecabinetdemacassar.herokuapp.com', 'cabinetdemacassar.herokuapp.com', "web-production-57c1.up.railway.app"]
 
