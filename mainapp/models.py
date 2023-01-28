d81b3b7bfb8f5cd68d9e392b61e4dc58d274ee26
Pavec Philippe <philippe.pavec@student-cs.fr>
2023-01-28 12:50:21 +0100

    modification ordre de clém

--- mainapp/models.py
+++ mainapp/models.py
@@ -19,12 +19,13 @@ class Contact(models.Model):
 class Furniture(models.Model):
 	title = models.CharField(max_length=35, verbose_name='titre (30 caractère max')
 	image =  models.ImageField(null=True, blank=True, verbose_name='image_liste (plutot carré)')
+	alt = models.CharField(max_length=255, verbose_name="texte alternatif à l'image", blank=True)
 	description = models.TextField(verbose_name = 'description', null=True, blank=True)
 	date = models.DateTimeField(blank=True)
 	keywords = models.CharField(null=True, max_length=255, verbose_name='mots clé à afficher en plus de catgorie et caractérisitique', blank=True)
 	for_sale = models.BooleanField()
 	category = models.CharField(max_length=42, verbose_name='catégorie : creation/restauration') #faire un form avec une liste déroulante avec restauration/création
-	caracteristic = models.CharField(max_length=255, verbose_name='caractéristiques : massif plaque // marqueterie sculpture boite_et_coffret jeux meuble_a_secret') #faire de même un forme déroulant
+	caracteristic = models.CharField(max_length=255, verbose_name='caractéristiques : massif plaque boulle siege // marqueterie sculpture boite_et_coffret jeux bijoux') #faire de même un forme déroulant
 	price = models.IntegerField(null=True, blank=True) #si pas à vendre pas de prix
 	#Mettre peut-être d'autres images avec charfield image
 
